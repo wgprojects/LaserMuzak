66fcf7ad0651d8f5b068b2cab26714e840d46766
William Gibson <william.gibson.wg@gmail.com>
2015-04-02 22:15:38 -0400

    Hacked-together and working. With Igor, April 1 2015.

---
+++ DirectSoundDemo/PlayControls.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace DirectSoundDemo
+{
+    partial class PlayControls
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pbPause = new System.Windows.Forms.PictureBox();
+            this.pbPlay = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.pbRewind = new System.Windows.Forms.PictureBox();
+            this.pbFastForward = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPause)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlay)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRewind)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFastForward)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.pbPause, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pbPlay, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 4, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 5, 1);
+            this.tableLayoutPanel1.Controls.Add(this.trackBar1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pbRewind, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pbFastForward, 3, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(580, 100);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // pbPause
+            // 
+            this.pbPause.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbPause.Image = global::DirectSoundDemo.Properties.Resources.pause;
+            this.pbPause.Location = new System.Drawing.Point(103, 53);
+            this.pbPause.Name = "pbPause";
+            this.pbPause.Size = new System.Drawing.Size(44, 44);
+            this.pbPause.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPause.TabIndex = 13;
+            this.pbPause.TabStop = false;
+            this.pbPause.Click += new System.EventHandler(this.pbPause_Click);
+            this.pbPause.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox4_MouseDown);
+            this.pbPause.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox4_MouseUp);
+            // 
+            // pbPlay
+            // 
+            this.pbPlay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbPlay.Image = global::DirectSoundDemo.Properties.Resources.play;
+            this.pbPlay.Location = new System.Drawing.Point(53, 53);
+            this.pbPlay.Name = "pbPlay";
+            this.pbPlay.Size = new System.Drawing.Size(44, 44);
+            this.pbPlay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPlay.TabIndex = 12;
+            this.pbPlay.TabStop = false;
+            this.pbPlay.Click += new System.EventHandler(this.pbPlay_Click);
+            this.pbPlay.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pbPlay.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.hScrollBar1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(203, 53);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(274, 44);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Volume";
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.LargeChange = 100;
+            this.hScrollBar1.Location = new System.Drawing.Point(3, 16);
+            this.hScrollBar1.Maximum = 10000;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(268, 20);
+            this.hScrollBar1.SmallChange = 10;
+            this.hScrollBar1.TabIndex = 6;
+            this.hScrollBar1.Value = 10000;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(483, 53);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(94, 44);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Time";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "00:00/00:00";
+            // 
+            // trackBar1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.trackBar1, 6);
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.trackBar1.Location = new System.Drawing.Point(3, 3);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(574, 44);
+            this.trackBar1.TabIndex = 9;
+            // 
+            // pbRewind
+            // 
+            this.pbRewind.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbRewind.Image = global::DirectSoundDemo.Properties.Resources.backward;
+            this.pbRewind.Location = new System.Drawing.Point(3, 53);
+            this.pbRewind.Name = "pbRewind";
+            this.pbRewind.Size = new System.Drawing.Size(44, 44);
+            this.pbRewind.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbRewind.TabIndex = 10;
+            this.pbRewind.TabStop = false;
+            this.pbRewind.Click += new System.EventHandler(this.pbRewind_Click);
+            this.pbRewind.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbRewind_MouseDown);
+            this.pbRewind.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbRewind_MouseUp);
+            // 
+            // pbFastForward
+            // 
+            this.pbFastForward.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbFastForward.Image = global::DirectSoundDemo.Properties.Resources.forward;
+            this.pbFastForward.Location = new System.Drawing.Point(153, 53);
+            this.pbFastForward.Name = "pbFastForward";
+            this.pbFastForward.Size = new System.Drawing.Size(44, 44);
+            this.pbFastForward.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbFastForward.TabIndex = 11;
+            this.pbFastForward.TabStop = false;
+            this.pbFastForward.Click += new System.EventHandler(this.pbFastForward_Click);
+            this.pbFastForward.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pbFastForward.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // PlayControls
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 100);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PlayControls";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "PlayControls";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PlayControls_FormClosing);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPause)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlay)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRewind)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFastForward)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.PictureBox pbRewind;
+        private System.Windows.Forms.PictureBox pbPause;
+        private System.Windows.Forms.PictureBox pbPlay;
+        private System.Windows.Forms.PictureBox pbFastForward;
+
+
+    }
+}
---
+++ DirectSoundDemo/SynthThread.cs
@@ -0,0 +1,375 @@
+﻿using AudioSynthesis.Synthesis;
+using AudioSynthesis.Sequencer;
+using AudioSynthesis.Midi;
+using NAudio.Wave;
+using System.IO;
+using AudioSynthesis.Bank;
+using AudioSynthesis.Bank.Patches;
+using System;
+using System.Collections.Generic;
+
+namespace DirectSoundDemo
+{
+    public sealed class SynthThread : IDisposable
+    {
+        private Synthesizer synth;
+        private MidiFileSequencer mseq;
+        private DirectSoundOut direct_out;
+        private SynthWaveProvider synth_provider;
+
+        public SynthThread()
+        {
+            synth = new Synthesizer(Properties.Settings.Default.SampleRate, 2, Properties.Settings.Default.BufferSize, Properties.Settings.Default.BufferCount, Properties.Settings.Default.poly);
+            mseq = new MidiFileSequencer(synth);
+            synth_provider = new SynthWaveProvider(synth, mseq);
+            direct_out = new DirectSoundOut(Properties.Settings.Default.Latency);
+            direct_out.Init(synth_provider);
+        }
+        public PatchBank Bank
+        {
+            get { return synth.SoundBank; }
+        }
+        public SynthWaveProvider Provider
+        {
+            get { return synth_provider; }
+        }
+        public SynthWaveProvider.PlayerState State
+        {
+            get { return synth_provider.state; }
+        }
+        public Synthesizer Synth
+        {
+            get { return synth; }
+        }
+        public void LoadBank(string bankfile)
+        {
+            Stop();
+            synth.LoadBank(new MyFile(bankfile));
+        }
+        public bool SongLoaded()
+        {
+            return mseq.IsMidiLoaded;
+        }
+        public bool BankLoaded()
+        {
+            return synth.SoundBank != null;
+        }
+        public bool SequencerStarted()
+        {
+            return mseq.IsPlaying;
+        }
+        public void UnloadSong()
+        {
+            if (!mseq.IsPlaying)
+                mseq.UnloadMidi();
+        }
+        public void LoadSong(string fileName)
+        {
+            
+            if (!mseq.IsPlaying)
+            {
+                var mf = new MidiFile(new MyFile(fileName));
+                mseq.LoadMidi(mf);
+                string path = Path.Combine(Path.GetDirectoryName(fileName), Path.GetFileNameWithoutExtension(fileName)+".muzak");
+                
+                SaveMuzak(mf, path);
+            }
+                
+
+
+            
+        }
+
+        private void SaveMuzak(MidiFile mf, string MuzakFile)
+        {
+            int ticksPerBeat = 96;
+            if(mf.TimingStandard == MidiFile.TimeFormat.FamesPerSecond)
+            {
+                throw new NotImplementedException();
+            }
+            else if (mf.TimingStandard == MidiFile.TimeFormat.TicksPerBeat)
+            {
+                ticksPerBeat = mf.Division;
+            }
+
+
+
+            
+            //notes start on 1/4 beat
+            //duration?
+            
+            using(StreamWriter op = new StreamWriter(MuzakFile))
+            {
+                Dictionary<int, List<AudioSynthesis.Midi.Event.MidiEvent>> eventByChannel = new Dictionary<int, List<AudioSynthesis.Midi.Event.MidiEvent>>();
+
+                List<string> axes = new List<string>(){":W", ":Y", ":X"};
+
+
+                foreach(var track in mf.Tracks)
+                {
+                   
+                    op.WriteLine("######################################################");
+                    op.WriteLine("#Reading Track");
+                    op.WriteLine(String.Format("# Active channels: {0}", track.ActiveChannels));
+                    op.WriteLine(String.Format("# DrumInstruments: {0}", track.DrumInstruments));
+                    op.WriteLine(String.Format("# EndTime: {0}", track.EndTime));
+                    op.WriteLine(String.Format("# Instruments: {0}", track.Instruments));
+                    op.WriteLine(String.Format("# NoteOnCount: {0}", track.NoteOnCount));
+
+
+                    foreach(var e in track.MidiEvents)
+                    {
+                        if( e.Command == (int)MidiEventTypeEnum.PitchBend)
+                        {
+                            Console.WriteLine(String.Format("#\t\t\t\t{0}: {1} d1:{2} d2:{3}", e.Channel, e.Command, e.Data1, e.Data2));
+                        }
+                        if (e.Command == (int)MidiEventTypeEnum.NoteOff || e.Command == (int)MidiEventTypeEnum.NoteOn)
+                        {
+                            if (!eventByChannel.ContainsKey(e.Channel))
+                                eventByChannel.Add(e.Channel, new List<AudioSynthesis.Midi.Event.MidiEvent>());
+
+                            eventByChannel[e.Channel].Add(e);
+                        }
+                        
+                    }
+                }
+
+                op.WriteLine("######################################################");
+                op.WriteLine("#Writing events by channel:");
+
+                foreach(int channel in eventByChannel.Keys)
+                {
+                    var events = eventByChannel[channel];
+
+                    op.WriteLine("######################################################");
+                    op.WriteLine(String.Format("#Channel: {0}", channel));
+                    if (axes.Count > 0)
+                    {
+                        op.WriteLine(axes[0]);
+                        axes.RemoveAt(0);
+                    }
+
+                    int lastOctave = -1;
+
+                    string currentNote = null;
+                    int currentMidiNote = -1;
+                   // int duration_ticks = 0;
+
+                    bool silence = true;
+
+                    int sum = 0;
+                    int lastTime = 0;
+
+                    foreach(var e in events)
+                    {
+                        //Console.WriteLine(String.Format("{0} + {1} = {2} vs {3}", sum, e.DeltaTime, sum + e.DeltaTime, e.AbsTime));
+                        //sum += e.DeltaTime;
+
+                        int octave;
+                        string note;
+                        
+
+                        if (e.Command == 0x90)
+                        { //NoteOn
+
+                            GetNote(e.Data1, out note, out octave);
+
+                            //duration_ticks = 0;
+
+                            //op.WriteLine(String.Format("ON: {0} {1} {2}", e.Data1, e.Data2, e.DeltaTime));
+                            op.WriteLine(String.Format("\t\t\t#ON: {0} - {1} {2} {3}", note, e.Data1, e.Data2, e.DeltaTime));
+
+                            if (silence)
+                            {
+                                WriteNote(op, ".", e.AbsTime, ref lastTime, ticksPerBeat);
+                                
+                                silence = false;
+                            }
+                            else //We're trying to start a new note without ending the last one.
+                            {
+
+                                WriteNote(op, currentNote, e.AbsTime, ref lastTime, ticksPerBeat);
+                                op.WriteLine("\t\t#REPEATED NOTE START");
+                            }
+
+
+                            if (octave != lastOctave)
+                            {
+                                op.WriteLine("o" + octave.ToString());
+                                lastOctave = octave;
+                            }
+
+                            currentNote = note;
+                            currentMidiNote = e.Data1;
+                            //duration_ticks = 0;
+                           
+                        }
+                        else if (e.Command == 0x80)
+                        {
+                            //NoteOff
+                            op.WriteLine(String.Format("\t\t\t#OFF: {0} {1} {2}", e.Data1, e.Data2, e.DeltaTime));
+
+
+                            if (silence || currentMidiNote != e.Data1)
+                            { //We previously tryied to start a new note without ending the last one. Skip!
+                                //duration_ticks += e.DeltaTime;
+                                //Do nothing
+                                op.WriteLine("\t\t#DO NOTHING");
+                            }
+                            else
+                            {
+                                WriteNote(op, currentNote, e.AbsTime, ref lastTime, ticksPerBeat);
+                                //if (duration_ticks > 0)
+                                //{
+                                //    WriteNote(op, currentNote, duration_ticks, ticksPerBeat);
+                                //}
+                                //else
+                                //{
+                                //    op.WriteLine("\t\t#SKIPPED WRITING NOTE WITH ZERO LENGTH! - " + currentNote);
+                                //}
+                                currentNote = null;
+                                currentMidiNote = -1;
+                                //duration_ticks = 0;
+                                silence = true; 
+                                //WriteNote(op, currentNote, duration_ticks, ticksPerBeat);
+
+                            }
+                        }
+                        else
+                        {
+                            op.WriteLine(String.Format("#\t\t\t\t{0}: {1} d1:{2} d2:{3} dt: {4}", e.Channel, e.Command, e.Data1, e.Data2, e.DeltaTime));
+                        }
+                    }
+                 
+                }
+            }
+        }
+
+
+
+        private void WriteNote(StreamWriter op, string currentNote, int absTime, ref int lastTime, int ticksPerBeat)
+        {
+            int duration = absTime - lastTime;
+            if (duration > 0)
+            {
+                op.WriteLine(String.Format("{0}{1}", currentNote, GetBeats(duration, ticksPerBeat)));
+                lastTime = absTime;
+            }
+            else
+            {
+                op.WriteLine("\t\t#Skipping empty note: " + currentNote);
+            }
+        }
+
+        private string GetBeats(int duration_ticks, int ticksPerBeat)
+        {
+            //double beatsSixteenths = 16 * duration_ticks / ticksPerBeat;
+           // int beatsInv = 16 * ticksPerBeat / duration_ticks;
+            return String.Format("{0}/{1}", ticksPerBeat, duration_ticks);
+        }
+
+        void GetNote(int midiFreq, out string note, out int octave)
+        {
+            string[] notes = new string[] { "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b" };
+
+            octave = (midiFreq / 12) - 1;
+
+            int noteIndex = midiFreq % 12;
+            note = notes[noteIndex];
+
+        }
+        public bool PlaySong(string fileName)
+        {
+            if (synth.SoundBank == null)
+                return false;
+            Stop();
+            mseq.LoadMidi(new MidiFile(new MyFile(fileName)));
+            mseq.Play();
+            Play();
+            return true;
+        }
+        public void Play()
+        {
+            if (synth.SoundBank == null)
+                return;
+            if (mseq.IsMidiLoaded && !mseq.IsPlaying)
+                mseq.Play();
+            if (synth_provider.state != SynthWaveProvider.PlayerState.Playing)
+            {
+                synth_provider.state = SynthWaveProvider.PlayerState.Playing;
+                if(direct_out.PlaybackState != PlaybackState.Playing)
+                    direct_out.Play();
+            }
+        }
+        public void Stop()
+        {
+            if (synth_provider.state != SynthWaveProvider.PlayerState.Stopped)
+            {
+                lock (synth_provider.lockObj)
+                {
+                    mseq.Stop();
+                    synth.NoteOffAll(true);
+                    synth.ResetSynthControls();
+                    synth.ResetPrograms();
+                    synth_provider.state = SynthWaveProvider.PlayerState.Stopped;
+                    synth_provider.Reset();
+                }
+            }
+        }
+        public void TogglePause()
+        {
+            if (synth_provider.state == SynthWaveProvider.PlayerState.Playing)
+            {
+                synth_provider.state = SynthWaveProvider.PlayerState.Paused;
+            }
+            else if (synth_provider.state == SynthWaveProvider.PlayerState.Paused)
+            {
+                synth_provider.state = SynthWaveProvider.PlayerState.Playing;
+            }
+        }
+        public void AddMessage(SynthWaveProvider.Message msg)
+        {
+            lock (synth_provider.lockObj)
+            {
+                synth_provider.msgQueue.Enqueue(msg);
+            }
+        }
+        public bool isMuted(int channel)
+        {
+            return mseq.IsChannelMuted(channel);
+        }
+        public bool isHoldDown(int channel)
+        {
+            return synth.GetChannelHoldPedalStatus(channel);
+        }
+        public string getProgramName(int channel)
+        {
+            return synth.GetProgramName(channel);
+        }
+        public string[] getProgramNames(int bankNumber)
+        {
+            string[] names = new string[PatchBank.BankSize];
+            for (int x = 0; x < names.Length; x++)
+            {
+                Patch p = synth.SoundBank.GetPatch(bankNumber, x);
+                if (p == null)
+                    names[x] = "Null";
+                else
+                    names[x] = p.Name;
+            }
+            return names;
+        }
+        public void Close()
+        {
+            Stop();
+            direct_out.Stop();
+            direct_out.Dispose();
+            synth.UnloadBank();
+            mseq.UnloadMidi();
+        }
+        public void Dispose()
+        {
+            Close();
+        }
+    }
+}
